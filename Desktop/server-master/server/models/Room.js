5e206b970e259f628e64e62e292953a3618a62b7
aarnav singh <aarnavsingh836@gmail.com>
2024-06-26 12:00:11 +0530

    Delete Desktop directory

--- Desktop/server-master/server/models/Room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-  roomNumber: { type: String, required: true },
-  roomType: { type: String, required: true },
-  // Add more room details here as needed
-});
-
-module.exports = mongoose.model('Room', roomSchema);
