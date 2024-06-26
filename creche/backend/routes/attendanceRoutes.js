4e653f8d52e30538f23d2f1c3ab7f86c0fb37d61
aarnav singh <aarnavsingh836@gmail.com>
2024-06-26 12:00:23 +0530

    Delete creche directory

--- creche/backend/routes/attendanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addAttendance, getAttendance } = require('../controllers/attendanceController');
-
-const router = express.Router();
-
-router.post('/', addAttendance);
-router.get('/', getAttendance);
-
-module.exports = router;
